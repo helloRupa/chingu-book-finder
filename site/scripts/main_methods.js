a983a7ffd6154a959e9e86c9851a34c7c3b5240b
Rupa Dhillon <rupa211984@gmail.com>
2019-03-03 21:15:13 -0800

    Get user input, fetch book data, log to console

--- site/scripts/main_methods.js
+++ site/scripts/main_methods.js
@@ -1,1 +1,47 @@
-console.log('hi');
+const searchBar = (function(input_id) {
+    let searchTxt = document.getElementById(input_id).value.trim();
+    
+    let formatText = function() {
+        const regex = /[^a-zA-Z0-9 ]/gi;
+        const spaces = /\s/gi;
+        return searchTxt.replace(regex, '').replace(spaces, '%20');
+    };
+
+    return {
+        format: formatText
+    };
+});
+
+const bookData = {
+    url: 'https://www.googleapis.com/books/v1/volumes?q=',
+    options: '&fields=items(volumeInfo)&projection=lite',
+    // success: success,
+    // failure: failure,
+    timeout: 1
+};
+
+const fetchData = (function(options_obj, searchTxt){
+    let url = options_obj.url + searchTxt + options_obj.options;
+    // let options = options_obj;
+
+    fetch(url)
+	.then(response => {
+		if (!response.ok) throw Error (response.statusText);
+		return response.json();
+	}).then((data) => {
+		console.log(data);
+	}).catch(error => {
+        console.log(error);
+    });
+});
+
+
+document.getElementById('submit-search').onclick = function(e) {
+    e.preventDefault();
+    // console.log(searchBar('search-bar').format());
+    let searchTxt = searchBar('search-bar').format();
+    fetchData(bookData, searchTxt);
+};
+
+    
+    
